d66fd5c7d8175da7d0db6392c6b6236c7527988e
gerardo2600 <73725618+gerardo2600@users.noreply.github.com>
2021-06-16 18:39:51 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry:"./src/index.js",
     output:{
         filename:"bundle.js",
-        path: __dirname+"/dist"
+        path: __dirname+"/docs"
     },
     mode:"production",
     module:{
